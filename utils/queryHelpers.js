518963fd553e435267be487b672d5a696aed8dce
Karthik-Kundurthy <karthikkundurthy@gmail.com>
2023-01-02 22:12:48 -0500

    Final Changes

--- utils/queryHelpers.js
+++ utils/queryHelpers.js
@@ -77,6 +77,16 @@ const getAllEmployees = async () => {
   )
 };
 
+const getAllWorkers = async () => {
+  const connection = await getConnection();
+  // return connection.execute(
+  //   "select e.id,e.name,d.name department,e.address from employee e,department d where e.department=d.id order by e.id"
+  // );
+  return connection.execute(
+    "select * from workers;"
+  )
+};
+
 const getAllPilots = async () => {
   const connection = await getConnection();
   // return connection.execute(
@@ -94,6 +104,15 @@ const getAllLocations = async () => {
   // );
   return connection.execute(
     "select * from display_location_view;"
+  )
+};
+const getAllDrones = async () => {
+  const connection = await getConnection();
+  // return connection.execute(
+  //   "select e.id,e.name,d.name department,e.address from employee e,department d where e.department=d.id order by e.id"
+  // );
+  return connection.execute(
+    "select * from drones;"
   )
 };
 
@@ -136,10 +155,34 @@ const deleteIngredient = async (iname) => {
 
 
 
-const addDrone = async(id, tag, barcode, packages, price) => {
+const loadDrone = async(id, tag, barcode, packages, price) => {
   console.log("Creating Drone: " + id + ", " + tag + ", " + barcode + ", " + packages + ", " + price)
   const connection = await getConnection();
   return connection.execute('CALL load_drone(?, ?, ?, ?, ?)', [id, tag, barcode, packages, price])
+}
+
+const addDrone = async(load_drone_id, load_drone_tag, load_drone_fuel, load_drone_capacity, load_drone_sales, load_drone_flown_by) => {
+  console.log(load_drone_id, load_drone_tag,
+    load_drone_fuel,
+    load_drone_capacity,
+    load_drone_sales,
+    load_drone_flown_by)
+  const connection = await getConnection();
+  return connection.execute('CALL add_drone(?, ?, ?, ?, ?, ?)', [load_drone_id,
+    load_drone_tag,
+    load_drone_fuel,
+    load_drone_capacity,
+    load_drone_sales,
+    load_drone_flown_by])
+}
+
+
+
+
+const addRestaurant = async(rest_name, rest_rating, rest_spent, rest_location) => {
+  // console.log("Creating Drone: " + id + ", " + tag + ", " + barcode + ", " + packages + ", " + price)
+  const connection = await getConnection();
+  return connection.execute('CALL add_restaurant(?, ?, ?, ?)', [rest_name, parseInt(rest_rating), parseInt(rest_spent), rest_location])
 }
 
 const addOwner = async(username, fname, lname, address, bdate) => {
@@ -178,6 +221,143 @@ const removePilot = async(remove_pilot_username) => {
   return connection.execute('CALL remove_pilot_role(?)', [remove_pilot_username])
 }
 
+const removeIngredient = async(remove_ing_barcode) => {
+  const connection = await getConnection();
+  return connection.execute('CALL remove_ingredient(?)', [remove_ing_barcode])
+}
+
+const removeDrone = async(remove_drone_id, remove_drone_tag) => {
+  const connection = await getConnection();
+  return connection.execute('CALL remove_drone(?, ?)', [remove_drone_id, remove_drone_tag])
+}
+
+const addWorker = async(worker_username) => {
+  const connection = await getConnection();
+  console.log(worker_username)
+  return connection.execute('CALL add_worker_role(?)', [worker_username])
+}
+
+const addStartFunding = async(start_funding_owner,
+  start_funding_long_name) => {
+  const connection = await getConnection();
+  console.log(start_funding_owner,
+    start_funding_long_name)
+  return connection.execute('CALL start_funding(?, ?)', [start_funding_owner,
+    start_funding_long_name])
+}
+
+const addLocation = async(location_label, location_x, location_y, location_space) => {
+  const connection = await getConnection();
+  console.log(location_label, location_x, location_y, location_space)
+  return connection.execute('CALL add_location(?, ?, ?, ?)', [location_label, location_x, location_y, location_space])
+}
+
+
+const addIngredient = async(ingredient_barcode,
+  ingredient_iname,
+  ingredient_weight) => {
+  const connection = await getConnection();
+  console.log(ingredient_barcode,
+    ingredient_iname,
+    ingredient_weight)
+  return connection.execute('CALL add_ingredient(?, ?, ?)', [ingredient_barcode,
+  ingredient_iname,
+  parseInt(ingredient_weight)])
+}
+
+const addHireEmployee = async( hire_employee_username,
+  hire_employee_id) => {
+  const connection = await getConnection();
+  console.log( hire_employee_username,
+    hire_employee_id)
+  return connection.execute('CALL hire_employee(?, ?)', [ hire_employee_username,
+    hire_employee_id])
+}
+
+const addPurchaseIngredient = async( purchase_ing_long_name,
+  purchase_ing_id,
+  purchase_ing_tag,
+  purchase_ing_barcode,
+  purchase_ing_quantity) => {
+  const connection = await getConnection();
+  console.log(purchase_ing_long_name)
+  return connection.execute('CALL purchase_ingredient(?, ?, ?, ?, ?)', [ purchase_ing_long_name,
+    purchase_ing_id,
+    parseInt(purchase_ing_tag),
+    purchase_ing_barcode,
+    parseInt(purchase_ing_quantity)])
+}
+
+const addRefuelDrone = async(refuel_drone_id,
+  refuel_drone_tag,
+  refuel_drone_more_fuel) => {
+  const connection = await getConnection();
+  console.log(refuel_drone_id,
+    refuel_drone_tag,
+    refuel_drone_more_fuel)
+  return connection.execute('CALL refuel_drone(?, ?, ?)', [refuel_drone_id,refuel_drone_tag,refuel_drone_more_fuel])
+}
+
+const addTakeoverDrone = async(takeover_drone_username,
+  takeover_drone_ID,
+  takeover_drone_tag) => {
+  const connection = await getConnection();
+  console.log(takeover_drone_username,
+    takeover_drone_ID,
+    takeover_drone_tag)
+  return connection.execute('CALL takeover_drone(?, ?, ?)', [takeover_drone_username, takeover_drone_ID, takeover_drone_tag])
+}
+
+
+const addFireEmployee = async(fire_employee_username,
+  fire_employee_id) => {
+  const connection = await getConnection();
+  console.log(fire_employee_username,
+    fire_employee_id)
+  return connection.execute('CALL fire_employee(?, ?)', [fire_employee_username,
+    fire_employee_id])
+}
+
+const addJoinSwarm = async(join_swarm_id,
+  join_swarm_tag,
+  join_swarm_leader_tag) => {
+  const connection = await getConnection();
+  console.log(join_swarm_id,
+    join_swarm_tag,
+    join_swarm_leader_tag)
+  return connection.execute('CALL join_swarm(?, ?, ?)', [join_swarm_id,
+    join_swarm_tag,
+    join_swarm_leader_tag])
+}
+
+const addLeaveSwarm = async(leave_swarm_id,
+  leave_swarm_tag) => {
+  const connection = await getConnection();
+  console.log(leave_swarm_id,
+    leave_swarm_tag)
+  return connection.execute('CALL leave_swarm(?, ?)', [leave_swarm_id,
+    leave_swarm_tag])
+}
+
+const addFlyDrone = async(fly_drone_id,
+  fly_drone_tag,
+  fly_drone_destination) => {
+  const connection = await getConnection();
+  console.log(fly_drone_id,
+    fly_drone_tag,
+    fly_drone_destination)
+  return connection.execute('CALL fly_drone(?, ?, ?)', [fly_drone_id,
+    fly_drone_tag,
+    fly_drone_destination])
+}
+
+
+
+// const loadDrone = async(load_drone_id, load_drone_tag, load_drone_barcode, load_drone_more_packages, load_drone_price) => {
+//   const connection = await getConnection();
+//   return connection.execute('CALL remove_pilot_role(?, )', [remove_pilot_username])
+// }
+
 
 
 // const getAllDepartments = async () => {
@@ -196,7 +376,7 @@ module.exports = {
   getAllServices,
   addPilot,
   // deleteIngredient,
-  addDrone,
+  loadDrone,
   addEmployee,
   hireEmployee,
   addOwner,
@@ -207,5 +387,23 @@ module.exports = {
   getAllEmployees,
   getAllPilots,
   getAllLocations,
-  removePilot
+  removePilot,
+  addRestaurant,
+  addWorker,
+  getAllWorkers,
+  removeIngredient,
+  getAllDrones,
+  removeDrone,
+  addLocation,
+  addIngredient,
+  addHireEmployee,
+  addStartFunding,
+  addPurchaseIngredient,
+  addTakeoverDrone,
+  addRefuelDrone,
+  addFireEmployee,
+  addJoinSwarm,
+  addLeaveSwarm,
+  addFlyDrone,
+  addDrone
 };
